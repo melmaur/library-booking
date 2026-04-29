d4cb266fad44cfcf59a1e63de8bdc9c4f4b2b5f8
melmaur <143796035+melmaur@users.noreply.github.com>
2026-04-29 10:25:56 +0200

    feat: config application.properties and LibraryBookingApplication.java

--- library-booking/src/main/java/library_booking/LibraryBookingApplication.java
+++ library-booking/src/main/java/library_booking/LibraryBookingApplication.java
@@ -2,12 +2,17 @@ package library_booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class DemoApplication {
+@ComponentScan(basePackages = {"library_booking", "controller", "service", "repository", "model"})
+@EntityScan(basePackages = "model")
+@EnableJpaRepositories(basePackages = "repository")
+public class LibraryBookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LibraryBookingApplication.class, args);
 	}
-
 }
